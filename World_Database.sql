2029b1d1f559237ba0739b19f1b2ff52595469ab
MichaelRiina <michaelriina@hotmail.com>
2020-05-09 16:02:24 -0400

    Add files via upload

---
+++ World_Database.sql
@@ -0,0 +1,18 @@
+
+
+create table World(
+	Country	 varchar,
+	Year	 int,
+	Health_Spending_by_GDP	 varchar,
+	Health_Spending_USD_by_Capita	 varchar,
+	Population	 varchar,
+	percent_of_world	 varchar,
+	Total_Cases	 varchar,
+	Total_Deaths	 varchar,
+	Active_Cases	 varchar,
+	Cases_by_1M_Population	 varchar,
+	Tests_by_1M_Population	 varchar
+
+)
+
+select * from world
